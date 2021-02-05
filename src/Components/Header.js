1efd9193f01a10a6e1a0f8d5d4f65ed56eb59dcd
Robert Gregg <hi@robertgregg.co.uk>
2021-02-05 21:39:35 +0000

    Added Header Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 import logo from "../images/logo.png";
 import "../css/Header.css";
+import { Apps, ExpandMore, HelpOutline, Settings } from "@material-ui/icons";
+import { Avatar } from "@material-ui/core";
 
 const Header = () => {
   return (
@@ -9,8 +12,21 @@ const Header = () => {
         <img src={logo} alt="" />
         <span>Drive</span>
       </div>
-      <div className="header__search"></div>
-      <div className="header__icons"></div>
+      <div className="header__searchContainer">
+        <div className="header__searchBar">
+          <SearchIcon />
+          <input type="text" placeholder="Search in Drive..." />
+          <ExpandMore />
+        </div>
+      </div>
+      <div className="header__icons">
+        <span>
+          <HelpOutline />
+          <Settings />
+        </span>
+        <Apps />
+        <Avatar />
+      </div>
     </div>
   );
 };
