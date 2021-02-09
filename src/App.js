45c28f1d4677eb36225cbe7067a71ee57d273919
Robert Gregg <hi@robertgregg.co.uk>
2021-02-09 21:10:17 +0000

    Added login element

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
+import { useState } from "react";
 import FilesView from "./Components/FilesView";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 import SideIcons from "./Components/SideIcons";
+import logo from "./images/logo.png";
 import "./css/App.css";
+import { auth, provider } from "./firebase";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = () => {
+    if (!user) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+          console.log(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (user) {
+      auth
+        .signOut()
+        .then(() => {
+          setUser(null);
+        })
+        .catch((err) => alert(err.message));
+    }
+  };
+
   return (
     <div className="app">
-      <Header />
-      <div className="app__main">
-        <Sidebar />
-        <FilesView />
-        <SideIcons />
-      </div>
+      {user ? (
+        <>
+          <Header userPhoto={user.photoURL} />
+          <div className="app__main">
+            <Sidebar />
+            <FilesView />
+            <SideIcons />
+          </div>
+        </>
+      ) : (
+        <div className="app__login">
+          <img src={logo} alt="Google drive" />
+          <button onClick={handleLogin}>Log in to Google Drive</button>
+        </div>
+      )}
     </div>
   );
 }
